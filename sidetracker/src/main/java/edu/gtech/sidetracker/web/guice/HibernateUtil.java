8a5a75b10f7c06376e21fb922a9d44bae53ac576
Karl Leswing <karl.leswing@schrodinger.com>
2015-04-21 18:39:30 -0400

    Imports cleanup

--- sidetracker/src/main/java/edu/gtech/sidetracker/web/guice/HibernateUtil.java
+++ sidetracker/src/main/java/edu/gtech/sidetracker/web/guice/HibernateUtil.java
@@ -1,11 +1,9 @@
 package edu.gtech.sidetracker.web.guice;
 
 
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.PreparedStatement;

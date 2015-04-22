9871957c17f248ec712124aa09fe051b494501d6
Karl Leswing <lilleswing@gmail.com>
2015-04-21 22:39:57 -0400

    Updated to add timeout on connection to FHIR server

--- sidetracker/src/main/java/edu/gtech/sidetracker/web/fhir/FhirClient.java
+++ sidetracker/src/main/java/edu/gtech/sidetracker/web/fhir/FhirClient.java
@@ -7,6 +7,7 @@ import edu.gtech.sidetracker.web.model.AppUser;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URL;
 
 
@@ -27,7 +28,12 @@ public class FhirClient {
             }
             final String fullUrl = this.url + MEDICATION_PRESCRIPTION + appUser.getFhirId();
             final URL oracle = new URL(fullUrl);
-            final BufferedReader in = new BufferedReader(new InputStreamReader(oracle.openStream()));
+            HttpURLConnection huc = (HttpURLConnection) oracle.openConnection();
+            HttpURLConnection.setFollowRedirects(false);
+            huc.setConnectTimeout(15 * 1000);
+            huc.setRequestMethod("GET");
+            huc.connect();
+            final BufferedReader in = new BufferedReader(new InputStreamReader(huc.getInputStream()));
 
             final StringBuilder sb = new StringBuilder();
             String inputLine;

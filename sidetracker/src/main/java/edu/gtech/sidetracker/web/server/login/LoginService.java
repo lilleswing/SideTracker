67865cced2e982a41b1b75b784d3c422c9391f84
Karl Leswing <lilleswing@gmail.com>
2015-04-21 22:28:08 -0400

    all linked up but not tested

--- sidetracker/src/main/java/edu/gtech/sidetracker/web/server/login/LoginService.java
+++ sidetracker/src/main/java/edu/gtech/sidetracker/web/server/login/LoginService.java
@@ -4,18 +4,28 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
 import com.google.inject.Inject;
+import com.google.inject.Provider;
 import com.google.inject.Singleton;
+import edu.gtech.sidetracker.web.fhir.PatientUpdater;
+import edu.gtech.sidetracker.web.guice.RequestState;
 
 @Path("/login")
 @Singleton
 public class LoginService {
 
+    private final Provider<RequestState> requestStateProvider;
+    private final PatientUpdater patientUpdater;
+
     @Inject
-    public LoginService() {
+    public LoginService(final Provider<RequestState> requestStateProvider,
+                        final PatientUpdater patientUpdater) {
+        this.requestStateProvider = requestStateProvider;
+        this.patientUpdater = patientUpdater;
     }
 
     @GET
     public String login() {
+        patientUpdater.update(requestStateProvider.get().getAppUser());
         return "Logged In";
     }
 }

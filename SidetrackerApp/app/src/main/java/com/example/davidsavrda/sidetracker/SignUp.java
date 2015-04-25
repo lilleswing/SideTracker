57e1d2d7824079b477e6543c79610ebca6ed0584
muglump <korahel@gmail.com>
2015-04-24 21:17:01 -0400

    Everything should be switched over, including SignUp

--- SidetrackerApp/app/src/main/java/com/example/davidsavrda/sidetracker/SignUp.java
+++ SidetrackerApp/app/src/main/java/com/example/davidsavrda/sidetracker/SignUp.java
@@ -2,6 +2,7 @@ package com.example.davidsavrda.sidetracker;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -9,9 +10,16 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
+import com.example.davidsavrda.sidetracker.client.RestClient;
+import com.example.davidsavrda.sidetracker.model.WsAppUser;
+import com.example.davidsavrda.sidetracker.model.WsMedication;
+
+import java.util.List;
+
 
 public class SignUp extends ActionBarActivity {
     //Not sure what the URL will be for the sign up yet
+    private WsAppUser user;
     public final static String apiURL = "localhost:8080/signUp?";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,16 +52,38 @@ public class SignUp extends ActionBarActivity {
 
 
     public void signUpPressed(View v){
-        //Make the call here
-        //After the call if there is an error
-        ((TextView) findViewById(R.id.username)).setText("");
-        ((TextView) findViewById(R.id.Password)).setText("");
-        ((TextView) findViewById(R.id.confirmPassword)).setText("");
-        //Otherwise we go to the main view
         Context context = getApplicationContext();
-        Intent intent = new Intent(context, MainActivity.class);
-        intent.putExtra("Username", ((TextView) findViewById(R.id.username)).getText().toString());
-        startActivity(intent);
+        final WsAppUser newUser = new WsAppUser();
+        newUser.setUserName(((TextView) findViewById(R.id.username)).getText().toString());
+        newUser.setPassword(((TextView) findViewById(R.id.username)).getText().toString());
+
+        final AsyncTask<Object, Void, WsAppUser> isLoggedIn = new AsyncTask<Object, Void, WsAppUser>() {
+            @Override
+            protected WsAppUser doInBackground(Object... params) {
+                return RestClient.createUser(newUser);
+            }
+
+            @Override
+            protected void onPostExecute(WsAppUser result) {
+
+                user = result;
+            }
+
+        }.execute();
+
+        if(user == null) {
+            ((TextView) findViewById(R.id.username)).setText("");
+            ((TextView) findViewById(R.id.Password)).setText("");
+            ((TextView) findViewById(R.id.confirmPassword)).setText("");
+        }
+        //Otherwise we go to the main view
+        else {
+            Intent intent = new Intent(context, MainActivity.class);
+            startActivity(intent);
+            intent.putExtra("Username", ((TextView) findViewById(R.id.username)).getText().toString());
+            intent.putExtra("Password", ((TextView) findViewById(R.id.Password)).getText().toString());
+            startActivity(intent);
+        }
     }
 
     public void cancelPressed(View v){
